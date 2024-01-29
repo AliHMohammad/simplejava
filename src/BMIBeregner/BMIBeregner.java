9faf6be63cb11d550437e926357c85a70db44369
AliHMohammad <alihmohammad@hotmail.com>
2024-01-29 16:53:20 +0100

    BMI-beregner

---
+++ src/BMIBeregner/BMIBeregner.java
@@ -0,0 +1,28 @@
+package BMIBeregner;
+
+import java.util.Scanner;
+
+public class BMIBeregner {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Velkommen til BMI-beregneren");
+
+        System.out.println("Indtast din vægt i kg:");
+        double weight = Double.parseDouble(scanner.nextLine());
+        System.out.println("Indtast din højde i meter:");
+        double height = Double.parseDouble(scanner.nextLine());
+
+        if (height > 100) {
+            height /= 100;
+        }
+
+        double BMI = calculateBMI(weight, height);
+
+        System.out.println("Du har en BMI på " + BMI);
+    }
+
+    public static double calculateBMI(double weight, double height) {
+        return weight / (height * height);
+    }
+}
