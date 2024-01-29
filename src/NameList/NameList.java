17a6369861931ec3be920a1cd93d39cbdc98832c
AliHMohammad <alihmohammad@hotmail.com>
2024-01-29 20:20:31 +0100

    namelist done

---
+++ src/NameList/NameList.java
@@ -0,0 +1,15 @@
+package NameList;
+
+public class NameList {
+
+    public static void main(String[] args) {
+        String[] names = {"Harry", "Ron", "Hermione", "Neville", "Draco"};
+
+        for (String name :
+                names) {
+            System.out.println(name);
+        }
+    }
+
+
+}
